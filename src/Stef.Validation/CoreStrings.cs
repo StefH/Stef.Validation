ac41e247ddb4902091312de8055c6f4ed86a2212
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-10-27 09:25:48 +0000

    Return values should be non-null

---
+++ src/Stef.Validation/CoreStrings.cs
@@ -0,0 +1,21 @@
+﻿// Based on https://github.com/aspnet/EntityFramework/blob/dev/src/Microsoft.EntityFrameworkCore/Properties/CoreStrings.resx
+namespace Stef.Validation
+{
+    internal static class CoreStrings
+    {
+        public static string ArgumentPropertyNull(string property, string argument)
+        {
+            return $"The property '{property}' of the argument '{argument}' cannot be null.";
+        }
+
+        public static string ArgumentIsEmpty(string argumentName)
+        {
+            return $"The string argument '{argumentName}' cannot be empty.";
+        }
+
+        public static string CollectionArgumentIsEmpty(string argumentName)
+        {
+            return $"The collection argument '{argumentName}' must contain at least one element.";
+        }
+    }
+}
