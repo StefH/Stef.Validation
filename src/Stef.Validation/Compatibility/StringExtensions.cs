fa330b11380f591c7e73b8690a5e41bb72092ea3
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2021-10-27 12:47:19 +0200

    Return values should be non-null (#2)
    * Return values should be non-null

---
+++ src/Stef.Validation/Compatibility/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System
+{
+    internal static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+#if !(NET20 || NET35)
+            return string.IsNullOrWhiteSpace(value);
+#else
+            if (value == null)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (!char.IsWhiteSpace(value[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+#endif
+        }
+    }
+}
---
+++ src/Stef.Validation/Guard.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+// Based on https://github.com/dotnet/efcore/blob/main/src/Shared/Check.cs
+namespace Stef.Validation
+{
+    [DebuggerStepThrough]
+    public static class Guard
+    {
+        public static T Condition<T>(T? value, [ValidatedNotNull] Predicate<T?> predicate, [ValidatedNotNull] string parameterName)
+        {
+            NotNull(predicate, nameof(predicate));
+            var result = NotNull(value, nameof(value));
+
+            if (!predicate(value))
+            {
+                NotNullOrEmpty(parameterName, nameof(parameterName));
+
+                throw new ArgumentOutOfRangeException(parameterName);
+            }
+
+            return result;
+        }
+
+        public static T NotNull<T>(T? value, [ValidatedNotNull] string parameterName)
+        {
+            if (value is null)
+            {
+                NotNullOrEmpty(parameterName, nameof(parameterName));
+
+                throw new ArgumentNullException(parameterName);
+            }
+
+            return value;
+        }
+
+        public static T NotNull<T>(T? value, [ValidatedNotNull] string parameterName, [ValidatedNotNull] string propertyName)
+        {
+            if (value is null)
+            {
+                NotNullOrEmpty(parameterName, nameof(parameterName));
+                NotNullOrEmpty(propertyName, nameof(propertyName));
+
+                throw new ArgumentException(CoreStrings.ArgumentPropertyNull(propertyName, parameterName));
+            }
+
+            return value;
+        }
+
+        public static IEnumerable<T> NotNullOrEmpty<T>(IEnumerable<T>? value, [ValidatedNotNull] string parameterName)
+        {
+            IEnumerable<T> result = NotNull(value, parameterName);
+
+            if (result.Count() == 0)
+            {
+                NotNullOrEmpty(parameterName, nameof(parameterName));
+
+                throw new ArgumentException(CoreStrings.CollectionArgumentIsEmpty(parameterName));
+            }
+
+            return result;
+        }
+
+        public static string NotNullOrEmpty(string? value, [ValidatedNotNull] string parameterName)
+        {
+            string result = NotNull(value, parameterName);
+
+            if (string.IsNullOrEmpty(result))
+            {
+                throw new ArgumentException(CoreStrings.ArgumentIsEmpty(parameterName));
+            }
+
+            return result;
+        }
+
+        public static string NotNullOrWhiteSpace(string? value, [ValidatedNotNull] string parameterName)
+        {
+            string result = NotNull(value, parameterName);
+
+            if (result.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentException(CoreStrings.ArgumentIsEmpty(parameterName));
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<T> HasNoNulls<T>(IEnumerable<T?>? value, [ValidatedNotNull] string parameterName) where T : class
+        {
+            IEnumerable<T?> nonNullValue = NotNull(value, parameterName);
+
+            if (nonNullValue.Any(e => e is null))
+            {
+                NotNullOrEmpty(parameterName, nameof(parameterName));
+
+                throw new ArgumentException(parameterName);
+            }
+
+            return nonNullValue.Cast<T>();
+        }
+    }
+}
