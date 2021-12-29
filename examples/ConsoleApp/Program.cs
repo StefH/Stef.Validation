6d0620d0517d033912d627daefcc06e39e63f670
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-12-29 17:11:12 +0100

    .

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Stef.Validation;
 
 namespace ConsoleApp
 {
@@ -6,27 +7,75 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var s = "";
-            s = null;
+            var actions = new Action[]
+            {
+                () =>
+                {
+                    Program? p = null;
+                    Guard.NotNull(p);
+                },
+                () =>
+                {
+                    Guard.NotNull((Program?)null);
+                },
+                () =>
+                {
+                    string s = "";
+                    Guard.NotNullOrEmpty(s);
+                },
+                () =>
+                {
+                    Guard.NotNullOrEmpty("");
+                },
+                () =>
+                {
+                    string s = "";
+                    Guard.NotNullOrWhiteSpace(s);
+                },
+                () =>
+                {
+                    Guard.NotNullOrWhiteSpace("");
+                },
+                () =>
+                {
+                    int intValue = 1;
+                    Guard.Condition(intValue, i => i > 5);
+                },
+                () =>
+                {
+                    Guard.Condition(1, i => i > 5);
+                },
+                () =>
+                {
+                    int[] intValues = Array.Empty<int>();
+                    Guard.NotNullOrEmpty(intValues);
+                },
+                () =>
+                {
+                    Guard.NotNullOrEmpty(Array.Empty<int>());
+                },
+                () =>
+                {
+                    var intValuesWithNullValue = new[] { (int?) 1, null };
+                    Guard.HasNoNulls(intValuesWithNullValue);
+                },
+                () =>
+                {
+                    Guard.HasNoNulls(new[] { (int?) 1, null });
+                }
+            };
 
-            try
+            foreach (var action in actions)
             {
-                new Class1().Test(s);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-
-            Console.WriteLine("\r\n------------------------------------------------------\r\n");
-
-            try
-            {
-                new Class1().TestNotNullOrEmpty("");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
+                try
+                {
+                    action();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine();
+                }
             }
         }
     }
