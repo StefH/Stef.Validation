63c6381dfc5a5faa884e9a64630502089c8825f6
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-01-20 12:14:45 +0100

    Add Stef.Validation.Options project

---
+++ src/Stef.Validation.Options/DataAnnotationOptionsValidator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Stef.Validation.Options;
+
+public static class DataAnnotationOptionsValidator<TOptions> where TOptions : class, new()
+{
+    private static readonly string Name = typeof(TOptions).Name;
+
+    public static void ValidateAndThrow(TOptions options)
+    {
+        if (options == null)
+        {
+            throw new OptionsValidationException(Name, typeof(TOptions), new[] { $"The {Name} object is null." });
+        }
+
+        var optionsValidator = new DataAnnotationValidateOptions<TOptions>(Name);
+        var validateResult = optionsValidator.Validate(Name, options);
+        if (validateResult.Failed)
+        {
+            throw new OptionsValidationException(Name, typeof(TOptions), validateResult.Failures);
+        }
+    }
+}
